928707caa2420db18e71031b3488e9a920b2dbd3
git-mhasan <mahadi2020dev@gmail.com>
2022-05-01 14:07:12 +0600

    initial backend connection ok.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+
 const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/user', (req, res) => {
+    res.send({ name: "mahadi", age: 23 });
+})
+
+app.listen(port, () => {
+    console.log("listening to port: ", port);
+})
