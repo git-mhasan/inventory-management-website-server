3a4001df47360c114d415698c53516b1e29529a5
git-mhasan <mahadi2020dev@gmail.com>
2022-05-02 10:55:08 +0600

    database updated for products

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,9 +9,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/user', (req, res) => {
-    res.send({ name: "mahadi", age: 23 });
-})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1moqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('greenTechDB').collection("products");
+
+        app.post('/product', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+            // console.log({ products });
+        });
+
+    }
+    finally {
+
+        console.log({ connection: "closed" });
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log("listening to port: ", port);
