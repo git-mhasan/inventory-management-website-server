232b4fd314cd25abebdac9dfabded0f83c2ca135
git-mhasan <mahadi2020dev@gmail.com>
2022-05-02 13:00:25 +0600

    deploying in heroku

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1moqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -41,7 +39,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
+app.get("/", (req, res) => {
+    res.send('server is running.');
+})
 app.listen(port, () => {
     console.log("listening to port: ", port);
 })
