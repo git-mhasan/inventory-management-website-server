b88297a4eadcd2d87fda52a9358473542321d86b
git-mhasan <mahadi2020dev@gmail.com>
2022-05-07 15:51:48 +0600

    jwt implemented.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -8,6 +9,21 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send("unauthorized access!");
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send("Forbidden")
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1moqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -50,10 +66,13 @@ async function run() {
         });
 
         // Get all products
-        app.get('/product', async (req, res) => {
+        app.get('/product', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
             let query = {};
-            email ? query = { manager: email } : query = {}
+            if (!!email === true && decodedEmail === email) {
+                query = { manager: email }
+            }
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
@@ -65,10 +84,15 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
-            // const products = await cursor.toArray();
             res.send(product);
-            // console.log({ products });
         });
+
+        // Auth Api
+        app.post("/login", async (req, res) => {
+            const email = req.body;
+            const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send(accessToken);
+        })
 
     }
     catch {
